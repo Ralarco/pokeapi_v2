83f4ce17f8a0dc0ce23a28f0bdf180ee838b73b3
Rodrigo Alarcon <rodalarcon@gmail>
2023-07-20 17:27:55 -0400

    se creo el nav y setup inicial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import BuscadorHeader from './BuscadorHeader'
 
 function Header() {
   return (
-        <div className='flex flex-col sm:flex-row items-center justify-around gap-1 bg-slate-500'>
-            <h1 className='p-4 text-slate-300'>Poke<strong>API</strong></h1>
+        <div className='flex flex-col sm:flex-row items-center justify-around gap-1 bg-slate-500 p-2'>
+            <h1 className='text-slate-300'>Poke<strong>API</strong></h1>
             <BuscadorHeader />
         </div>
 
---
+++ src/components/NavCategorias.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavCategorias() {
+  return (
+    <nav className='p-4 bg-gray-900 text-cyan-100'>
+        <ul className='flex flex-row justify-evenly items-center max-w-xs mx-auto'>
+            <li><a href='#categoria/tecnologia'>Tecnología</a></li>
+            <li><a href='#categoria/tecnologia'>Tecnología</a></li>
+            <li><a href='#categoria/tecnologia'>Tecnología</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavCategorias
