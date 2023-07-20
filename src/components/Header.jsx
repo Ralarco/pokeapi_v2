98f490f5525e16b40c2909a15e56f804e9bb215d
RodrigoA <rodalarcon@gmail.com>
2023-07-20 09:27:09 -0400

    setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+        <div className='grid grid-cols-1 gap-2 bg-slate-500 md:grid-cols-2  '>
+            <h1 className='p-4 text-slate-300'>Poke<strong>API</strong></h1>
+
+        </div>
+
+  )
+}
+
+export default Header
